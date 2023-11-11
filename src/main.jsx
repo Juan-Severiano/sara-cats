092e040a2ecf36b7c20bfc8d931c85dc9763f0bf
Francisco Juan <juansoussev@gmail.com>
2023-11-11 11:53:35 -0300

    package adicionadas e sass criado

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import './styles/global.css'
+import './styles/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
